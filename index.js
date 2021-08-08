9eda8a10ef1b2ace011faa57ae17b7b5883fe32a
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-08-07 23:29:30 -0700

    add axios call to remote server

--- index.js
+++ index.js
@@ -7,8 +7,25 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-server.use("/", (req, res) => {
-    res.status(200).send("Server running");
+const axios = require("axios");
+const remoteEndpoint = process.env.URL;
+const responseProperty = process.env.RESPONSE_PROPERTY;
+
+server.use("/", async (req, res) => {
+
+    // remove first "/" character from req.url
+    if (process.env.REMOVE_LEADING_SLASH)
+        { req.url = req.url.slice(1); }
+
+    await axios.get(remoteEndpoint + req.url)
+        .then(response => {
+            console.log(response[responseProperty]);
+            res.status(200).json(response[responseProperty]);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(404).json("Not found.");
+        });
 })
 
 const PORT = process.env.PORT || 5000;
